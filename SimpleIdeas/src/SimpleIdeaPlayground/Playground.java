61537f0fae6322d90360c5143db94cb395f4644e
Lingxi Wu <lingxiwu1993@gmail.com>
2017-10-11 12:23:08 -0400

    Initial commit to local repo.

---
+++ SimpleIdeas/src/SimpleIdeaPlayground/Playground.java
@@ -0,0 +1,24 @@
+package SimpleIdeaPlayground;
+
+import Utilities.ParserUtility;
+import edu.stanford.nlp.pipeline.Annotation;
+
+public class Playground {
+
+	public static void main(String[] args) {
+
+		ParserUtility parserHelper = new ParserUtility();
+		
+	    // read some text in the text variable
+	    String text = "No incidence of vehicle collision should occur.";
+
+	    // create an empty Annotation just with the given text
+	    Annotation document = new Annotation(text);
+
+	    // run all Annotators on this text
+	    parserHelper.getPipeline().annotate(document);
+
+	    System.out.println('c');
+	}
+
+}
---
+++ SimpleIdeas/src/Utilities/ParserUtility.java
@@ -0,0 +1,115 @@
+package Utilities;
+
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+public class ParserUtility {
+
+	private enum SubjectTags {
+		nsubj, // nominal subject.
+		nsubjpass // passive nominal subject.
+	}
+	private enum BaseVerbTags {
+		VB, // Verb, base form
+		VBD, // Verb, past tense
+		VBG, // Verb, gerund/present participle
+		VBN, // Verb, past participle
+		VBP, // Verb, non-3rd person singular present  
+		VBZ // Verb, 3rd person singular present
+	}
+	
+	private Properties props;
+	private static StanfordCoreNLP pipeline;
+	
+	/**
+	 * Empty args Constructor.
+	 * @author lw2ef
+	 *
+	 */
+	public ParserUtility() {
+		// Set default props which is almost annotators.
+		props = new Properties();
+		props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		pipeline = new StanfordCoreNLP(props);
+	}
+	
+	/**
+	 * One constructor that can instantiate a pipeline object.
+	 * @author lw2ef
+	 *
+	 */
+	public ParserUtility(String propsString) {
+		// Make a pipeline object.
+		props = new Properties();
+		props.put("annotators", propsString);
+		pipeline = new StanfordCoreNLP(props);		
+	}
+	
+	
+	
+	/**
+	 * TO-DO:
+	 * Take an annotated String such as occur/VB (root) to extract the base verb "occur."
+	 * @param args
+	 */
+	public static String[] baseVerb(String dependencyAnnotatedLine) {
+		
+		String[] baseVerb = new String[2];
+		
+		for(BaseVerbTags bvt : BaseVerbTags.values()) {
+			if(dependencyAnnotatedLine.contains(bvt.toString())){
+				
+			}
+		}
+		
+		return baseVerb;
+	}
+	
+	/**
+	 * TO-DO:
+	 * @param dependencyLine
+	 * @return
+	 */
+	public static String[] nounSubject(String dependencyLine) {
+		String[] nounSubject = new String[2];
+		if(dependencyLine.contains(SubjectTags.nsubj.toString())) { 
+			nounSubject[0] = dependencyLine.trim();
+		}
+		
+		return nounSubject;
+	}
+	
+	/**
+	 * Take a word and return its lemma.
+	 * @param propsString
+	 */
+	public static String lemmatize(String word) {
+		String lemma = word;
+		Annotation doc = new Annotation(word);
+		pipeline.annotate(doc);
+		lemma = doc.get(TokensAnnotation.class).get(0).get(LemmaAnnotation.class);
+		
+		return lemma;
+		
+	}
+	
+	/*
+	 * Getters and Setters.
+	 */
+	public void setProps(String propsString) {
+		props.put("annotators", propsString);
+	}
+	
+	public Properties getProps() {
+		return props;
+	}
+	
+	public StanfordCoreNLP getPipeline() {
+		return pipeline;
+	}
+}
